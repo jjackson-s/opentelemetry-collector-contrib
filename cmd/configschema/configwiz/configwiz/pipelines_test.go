8d9714805223142f01bb3558688de4e47844ff0d
Jevon Jackson <jjackson@splunk.com>
2021-07-16 10:50:17 -0700

    updated singelpipeTest

--- cmd/configschema/configwiz/configwiz/pipelines_test.go
+++ cmd/configschema/configwiz/configwiz/pipelines_test.go
@@ -205,6 +205,18 @@ func TestPipelineWizardEmpty(t *testing.T) {
 	assert.Equal(t, expected, w.programOutput)
 }
 
+func TestSinglePipelineWizardFail(t *testing.T) {
+	w := fakeWriter{}
+	r := fakeReaderPipe{userInput: []string{"-1", ""}}
+	io := clio{w.write, r.read}
+	testFact := createTestFactories()
+	_, rpeOut := singlePipelineWizard(io, testFact)
+	expected := "Add pipeline (enter to skip)\n1: Metrics\n2: Traces\n> "
+	expected += "Invalid input. Try again.\n" + expected
+	assert.Equal(t, rpe{}, rpeOut)
+	assert.Equal(t, expected, w.programOutput)
+}
+
 func TestSinglePipelineWizardEmpty(t *testing.T) {
 	w := fakeWriter{}
 	r := fakeReaderPipe{userInput: []string{""}}
